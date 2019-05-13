dfbcc3ce9320011755019a8f87adc89bcfc784d9
tranduy1441997 <49270829+tranduy1441997@users.noreply.github.com>
2019-05-13 15:37:06 +0700

    7

--- app/src/main/java/com/hnam/recyclerview/ProfileActivity.kt
+++ app/src/main/java/com/hnam/recyclerview/ProfileActivity.kt
@@ -1,16 +1,16 @@
 package com.hnam.recyclerview
 
-import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
 import android.view.View
 import com.bumptech.glide.Glide
 import kotlinx.android.synthetic.main.activity_profile.*
 
 class ProfileActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile)
-
         getAndPutData()
     }
 
@@ -42,3 +42,4 @@ class ProfileActivity : AppCompatActivity() {
         }
     }
 }
+
